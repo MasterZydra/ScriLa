ec964352eb2eb9012d903f15ba031e061aab68fb
MasterZydra <david-hein196@web.de>
2023-10-03 21:16:23 +0200

    Transpiler: Add doc headers to bash helper functions

--- cmd/scrila/transpiler/tobashhelpers.go
+++ cmd/scrila/transpiler/tobashhelpers.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 )
 
+// Returns the given string wrapped in double quotes
 func strToBashStr(str string) string {
 	return fmt.Sprintf("\"%s\"", str)
 }
 
+// Returns the symbol of the given expr of kind Identifier
 func identNodeGetSymbol(expr ast.IExpr) string {
 	return ast.ExprToIdent(expr).GetSymbol()
 }
